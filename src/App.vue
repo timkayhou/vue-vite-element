febd5d8747f768e1b410c24e9691acdf41979d70
Expurgator <timkayhou@gmail.com>
2022-02-26 07:18:33 +0900

    🌈 style

--- src/App.vue
+++ src/App.vue
@@ -34,12 +34,13 @@ import BaseHeader from './components/layouts/BaseHeader.vue'
 
 <style>
 #app {
+  color: #2c3e50;
   font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
+
 .logo {
   width: 5%;
 }
